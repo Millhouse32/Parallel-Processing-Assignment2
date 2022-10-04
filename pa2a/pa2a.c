430ab162cb10676d4c391abdc33e4e2673313323
Nicholas Miller <nlm82@uakron.edu>
2022-10-04 16:16:59 -0400

    Add ability to take in M and N and output it to file

--- pa2a/pa2a.c
+++ pa2a/pa2a.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Hello world\n");
+    int m, n;
+
+    printf("Enter a vlaue for M (lower bound)\n");
+    scanf("%d", &m);
+    printf("Enter value for N (upper bound)\n");
+    scanf("%d", &n);
+
+    FILE* file;
+    file = fopen("performance.txt", "a");
+    fprintf(file, "Value for M (lower bound): %d\n", m);
+    fprintf(file, "Value for N (upper bound): %d\n", n);
+
+    fclose(file);
 }
