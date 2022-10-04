39a0ae46a16e06a21e08e02b778fb9e937bf298c
Nicholas Miller <nlm82@uakron.edu>
2022-10-04 16:30:48 -0400

    Add function to fill array with random numbers between M and N

--- pa2a/pa2a.c
+++ pa2a/pa2a.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+void Create_Array(int* m, int* n, int array[], int size);
 
 int main() {
     int m, n;
@@ -15,4 +18,27 @@ int main() {
     fprintf(file, "Value for N (upper bound): %d\n", n);
 
     fclose(file);
+
+    int arraySizes[] = { 1000, 2000, 4000, 8000, 16000 };
+
+    int size = 10;
+    int array[10];
+
+    Create_Array(&m, &n, array, size);
+}
+
+void Create_Array(int* m, int* n, int array[], int size) {
+    // seed random generator
+    srand ( time(NULL) );
+
+    for (int i = 0; i < size; i++) {
+        int num = (rand() % 
+        (*n-*m + 1)) + *m;
+        array[i] = num;
+    }
+
+    // print array
+    for (int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
 }
